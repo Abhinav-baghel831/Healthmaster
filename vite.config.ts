45d529c6840077b7078b2cf132c6462335a18ec1
Abhinav-baghel831 <prateek.baghel831@gmail.com>
2025-05-18 15:39:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: '0.0.0.0',  // This is required for Render to detect the port
+    port: 5173        // Optional: Vite default, Render overrides this anyway
+  }
 });
+
